fcd13eb6b07e3be4a5d505e540bb0d065d1096cf
Jonatan Claros <jona.cc.93@gmail.com>
2023-01-28 20:55:01 -0300

    minor changes login register

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -35,7 +35,7 @@ export const Login = (): any => {
 	return (
 		<section className="bg-gray-50 dark:bg-gray-900">
 			<div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-				<div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
+				<div className="xl:p-0 w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0">
 					<div className="space-y-4 p-6 sm:p-8 md:space-y-6">
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
 							Sign in to your account
